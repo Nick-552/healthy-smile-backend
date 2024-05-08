5e7c2f4b3be86d0d094592077ffa3a462d68c700
Nick-552 <TTchip552@ya.ru>
2024-05-08 21:43:27 +0300

    Added structure and dependencies

---
+++ src/main/java/ru/nick552/healthysmile/HealthySmileApplication.java
@@ -0,0 +1,13 @@
+package ru.nick552.healthysmile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthySmileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthySmileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nick552/healthysmile/HealthySmileApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nick552.healthysmile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthySmileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
